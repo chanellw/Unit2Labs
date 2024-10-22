570ab058bb7b2c158e5ddf00072d90f632fe4c68
Chanel Belvin <68121865+chanellw@users.noreply.github.com>
2024-10-22 17:21:13 +0000

    Solution for the first lab

--- Social.java
+++ Social.java
@@ -15,20 +15,25 @@ public class Social
 
 	public Social()
 	{
+		socialNum = "000-00-0000";
 	}
 
 	public Social(String soc)
 	{
+		socialNum = soc;
 	}
 
 
 	public void setWord(String w)
 	{
+		socialNum = w;
 	}
 
 	public void chopAndAdd()
 	{
 		String sub = socialNum;
+		sum = Integer.parseInt(sub.substring(0, sub.indexOf("-"))) + Integer.parseInt(sub.substring(sub.indexOf("-") + 1, sub.lastIndexOf("-") )) + Integer.parseInt(sub.substring(sub.lastIndexOf("-") + 1 ));
+		
 	}
 
 	public String toString()
--- SocialRunner.java
+++ SocialRunner.java
@@ -8,7 +8,21 @@ public class SocialRunner
 {
 	public static void main( String args[] )
 	{
-		//add test cases
+		Social testOne = new Social("456-56-234");
+		Social testtwo = new Social("1-1-1");
+		Social testThree = new Social("102-2-12");
+		testOne.chopAndAdd();
+		testtwo.chopAndAdd();
+		testThree.chopAndAdd();
+		System.out.println(testOne);
+		System.out.println(testtwo);
+		System.out.println(testThree);
+
+
+
+
+
+
 		
 	}
 }
